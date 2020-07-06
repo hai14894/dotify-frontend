e78550cc6ae6ed6baaa0d5c5b48337cebd959b7f
hai14894 <hohoanghai14894@gmail.com>
2020-07-06 15:48:48 +1000

    redirect to /

--- src/CreateSong.js
+++ src/CreateSong.js
@@ -1,16 +1,19 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import {Redirect} from 'react-router-dom'
 
 const CreateSong = () => {
     const [songTitle, setSongTitle] = useState("")
     const [songArtist, setSongArtist] = useState("")
+    const [isCreated, setisCreated] = useState(false)
     function createSong(){
         axios.post(`http://localhost:3000/songs`, {
         song:{
             title: songTitle,
             artist: songArtist
-        }
+            }
         })
+        .then(()=> setisCreated(true))
     }
     return (
         <div>
@@ -27,6 +30,7 @@ const CreateSong = () => {
             <button onClick={createSong}> 
                 Create Song
             </button>
+            {isCreated && <Redirect to="/" />}
         </div>
     )
 }
