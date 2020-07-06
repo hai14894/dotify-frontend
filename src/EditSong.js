63ca0a2e18cb53fd898589d67a505fb17288254e
hai14894 <hohoanghai14894@gmail.com>
2020-07-06 16:58:55 +1000

    finish the edit function

--- src/EditSong.js
+++ src/EditSong.js
@@ -1,15 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import {Link} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 
 const EditSong = (props) => {
     const [songTitle, setSongTitle] = useState("")
     const [songArtist, setSongArtist] = useState("")
 
-    const [song, setSong] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [errorMessage, setErrorMessage] = useState("")
-
+    const [isEdited , setIsEdited] = useState(false)
     useEffect(() => {
         axios.get(`http://localhost:3000/songs/${props.songId}`)
             .then(res => {
@@ -23,6 +22,20 @@ const EditSong = (props) => {
             })
     }, [])
 
+    function editSong(){
+        if(songTitle && songArtist){
+            axios.put(`http://localhost:3000/songs/${props.songId}`, {
+        song:{
+            title: songTitle,
+            artist: songArtist
+            }
+        })
+        .then(()=> setIsEdited(true))
+        }else{
+            setErrorMessage("Please fill out the field")
+        }
+        
+    }
     return (
         <div>
             {errorMessage && <h3>{errorMessage}</h3>}
@@ -39,7 +52,10 @@ const EditSong = (props) => {
                 </div> : 
                 <h2>Loading...</h2>
             }
-            
+            <button onClick={editSong}>
+                Edit Song
+            </button>
+            {isEdited && <Redirect to="/" />}
         </div>
     )
 }
